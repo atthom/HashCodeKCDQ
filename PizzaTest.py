bcf10ec7153c6de7fc22b56b1e4185bc3366308b
Thomas Jalabert <thom.jalabert@gmail.com>
2017-02-18 19:26:52 +0100

    implement somes classes

--- PizzaTest.py
+++ PizzaTest.py
@@ -1,5 +1,42 @@
 import networkx
 
+from docutils.parsers.rst.directives.html import meta
+
+
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Shape:
+    def __init__(self, longeur, largeur):
+        self.longeur = longeur
+        self.largeur = largeur
+
+    def size(self):
+        return self.longeur*self.largeur
+
+
+class Pizza:
+    def __init__(self, matrix, min, max):
+        self.matrix = matrix
+        self.min_ingredient = min
+        self.max_shape_size = max
+
+    def isValidShape(self, shape, coordinate):
+        nb_tomato = 0
+        for i in range(coordinate.x, coordinate.x + shape.longeur):
+            if matrix[i][coordinate.y] == 'T':
+                nb_tomato += 1
+
+        nb_mush = shape.size() - nb_tomato
+
+        if nb_mush> self.min_ingredient and nb_tomato>self.min_ingredient:
+            return True
+        else:
+            return False
+
+
 f = open("input_files/small.in", "r")
 
 #str = f.read()
@@ -12,25 +49,20 @@ for line in f.readlines():
         #matrix.append()
 
 
-meta = matrix[0]
+meta_list = matrix[0].split(' ')
 del matrix[0]
-
 print(matrix)
-
-meta_list = meta.split(' ')
-
 print(meta_list)
 
 L = len(matrix)
 l = len(matrix[0])
-matrix_zero = [L * [0] for _ in range(l)]
+matrix_zero = [L * [_] for _ in range(l)]
 
 print(matrix_zero)
 
-#for line in matrix:
-#    for char in line:
-#        if char == 'T':
-#            before =
+pizza = Pizza(matrix, meta_list[2], meta[3])
 
 
+#min_long = meta_list[2]
+#max_long =
 
