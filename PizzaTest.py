0cba8ac8ea8fa815f2c2c67b73e8b36278c1676d
Thomas Jalabert <thom.jalabert@gmail.com>
2017-02-18 21:33:05 +0100

    Using library networkX to compute the graph

--- PizzaTest.py
+++ PizzaTest.py
@@ -1,13 +1,15 @@
-import networkx
 import math
 import numpy
 
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class Coordinate:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
 
 class Shape:
     def __init__(self, longeur, largeur):
@@ -22,7 +24,7 @@ class Shape:
         return True
 
     def size(self):
-        return self.longeur*self.largeur
+        return self.longeur * self.largeur
 
     def __str__(self):
         return "{ longeur:" + str(self.longeur) + "," + " largeur: " + str(self.largeur) + "}"
@@ -37,25 +39,23 @@ class Pizza:
         self.width = int(list[0])
         L = len(matrix)
         l = len(matrix[0])
-        self.matrix_tomato = [L * [0] for _ in range(l)] # numpy.zeros((L, l))
+        self.matrix_tomato = [L * [0] for _ in range(l)]  # numpy.zeros((L, l))
         self.generate_array_tomato()
 
     def generate_array_tomato(self):
-        #L = len(matrix)
-       # l = len(matrix[0])
-       # self.matrix_tomato = numpy.zeros((L, l)) #  [L * [_] for _ in range(l)]
+        # L = len(matrix)
+        # l = len(matrix[0])
+        # self.matrix_tomato = numpy.zeros((L, l)) #  [L * [_] for _ in range(l)]
 
         nb_tomato = 0
         for i in range(0, self.long):
             for j in range(0, self.width):
                 if matrix[j][i] == 'T':
                     nb_tomato += 1
-                    self.matrix_tomato[i][j] = nb_tomato
+                self.matrix_tomato[i][j] = nb_tomato
 
         print("\nNb tomato :")
         print(self.matrix_tomato)
-
-
 
     def isValidSlice(self, shape, coordinate):
         nb_tomato = 0
@@ -65,7 +65,7 @@ class Pizza:
 
         nb_mush = shape.size() - nb_tomato
 
-        if nb_mush> self.min_ingredient and nb_tomato>self.min_ingredient:
+        if nb_mush > self.min_ingredient and nb_tomato > self.min_ingredient:
             return True
         else:
             return False
@@ -76,7 +76,6 @@ f = open("input_files/small.in", "r")
 matrix = []
 for line in f.readlines():
     matrix.append(line[:-1])
-
 
 meta_list = matrix[0].split(' ')
 del matrix[0]
@@ -92,11 +91,11 @@ min_long = math.floor(math.sqrt(float(meta_list[3])))
 
 all_shape = []
 
-min_size = int(meta_list[2])*2
+min_size = int(meta_list[2]) * 2
 max_size = int(meta_list[3])
 
-for i in range(1, max_size+1):
-    for j in range(1, max_size+1):
+for i in range(1, max_size + 1):
+    for j in range(1, max_size + 1):
         current_shape = Shape(i, j)
         if current_shape.isValidShape(min_size, max_size):
             all_shape.append(current_shape)
@@ -105,4 +104,64 @@ print("\nShapes acceptables :")
 for shape in all_shape:
     print(shape)
 
+print("sandbox pour trouver un sous graph complet")
+G = nx.DiGraph()
 
+G.add_edge('1', '2', weight=4)
+G.add_edge('1', '4', weight=4)
+G.add_edge('1', '7', weight=4)
+G.add_edge('1', '9', weight=4)
+
+G.add_edge('2', '6', weight=7)
+G.add_edge('2', '3', weight=7)
+G.add_edge('2', '1', weight=7)
+G.add_edge('2', '10', weight=7)
+
+G.add_edge('3', '5', weight=8)
+G.add_edge('3', '8', weight=8)
+G.add_edge('3', '6', weight=8)
+G.add_edge('3', '2', weight=8)
+G.add_edge('3', '10', weight=8)
+
+G.add_edge('4', '1', weight=6)
+G.add_edge('4', '7', weight=6)
+G.add_edge('4', '5', weight=6)
+
+G.add_edge('5', '8', weight=30)
+G.add_edge('5', '3', weight=30)
+G.add_edge('5', '4', weight=30)
+
+G.add_edge('6', '8', weight=2)
+G.add_edge('6', '3', weight=2)
+G.add_edge('6', '2', weight=2)
+G.add_edge('6', '10', weight=2)
+
+G.add_edge('7', '4', weight=4)
+G.add_edge('7', '1', weight=4)
+
+G.add_edge('8', '6', weight=2)
+G.add_edge('8', '3', weight=2)
+G.add_edge('8', '5', weight=2)
+G.add_edge('8', '10', weight=2)
+
+G.add_edge('9', '1', weight=5)
+
+G.add_edge('10', '2', weight=2)
+G.add_edge('10', '6', weight=2)
+G.add_edge('10', '3', weight=2)
+G.add_edge('10', '8', weight=2)
+
+pos = nx.spring_layout(G)  # positions for all nodes
+
+# nodes
+nx.draw_networkx_nodes(G, pos, node_size=700)
+
+# edges
+nx.draw_networkx_edges(G, pos, edgelist=G.edges(data=True),  width=6)
+
+# labels
+nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+
+plt.axis('off')
+plt.savefig("weighted_graph.png")  # save as png
+plt.show()
