e4dab5fd0f302b84da8644fabb2146de096bc566
unknown <ielatifi@NCEL25468.iis.amadeus.net>
2017-02-23 21:15:51 +0100

    Evaluator v1 to be debugged

--- Evaluator.py
+++ Evaluator.py
@@ -1,10 +1,10 @@
 import sys
-from collections import namedtuple
+from collections import namedtuple,defaultdict
 
 Cache = namedtuple('Cache', ['id', 'capacity'])
 Video = namedtuple('Video', ['id', 'size'])
 Endpoint = namedtuple('Endpoint', ['id', 'dataCenterLat','cachesLat'])
-RequestGroup = namedtuple('RequestGroup', ['nbRequests','video','endpoint'])
+RequestGroup = namedtuple('RequestGroup', ['nbRequests','idVideo','idEndpoint'])
 Inputs = namedtuple('Inputs', ['videos','endpoints','requestGroups','nbCaches','cacheCapa'])
 
 def parseInputs():
@@ -39,8 +39,9 @@ def parseSolution():
         videosByCache[idCache] = videos
     return videosByCache
 
-def checkValiditySolution(inputs, videosByCache):
+def checkValiditySolutionAndBuildCachesByVideo(inputs, videosByCache):
     videos, endpoints, requestGroups, nbCaches, cacheCapa = inputs
+    cachesByVideo = defaultdict()
     for idCache,videosInCache in videosByCache.items():
         assert(0 <= idCache < nbCaches)
         assert(len(videosInCache) <= len(videos))
@@ -48,13 +49,31 @@ def checkValiditySolution(inputs, videosByCache):
         for idVideo in videosInCache:
             assert (0 <= idVideo < len(videos))
             sumVidSizes += videos[idVideo].size
+            cachesByVideo[idVideo].append(idCache)
         assert(0 < sumVidSizes <= cacheCapa)
+    return cachesByVideo
 
+def scoreSolution(inputs, videosByCache):
+    videos, endpoints, requestGroups, nbCaches, cacheCapa = inputs
+    cachesByVideo = checkValiditySolutionAndBuildCachesByVideo(inputs, videosByCache)
+    sumGains = 0
+    sumNbReqs = 0
+    for nbRequests,idVideo,idEndpoint in requestGroups:
+        endpoint = endpoints[idEndpoint]
+        cachesLatenciesFromEndpt = []
+        for idCache in cachesByVideo[idVideo]:
+            cachesLatenciesFromEndpt.append(endpoint.cachesLat[idCache])
+        latToDatacenter = endpoint.dataCenterLat
+        minLatToCaches = min(cachesLatenciesFromEndpt)
+        minLatTotal = min(minLatToCaches,latToDatacenter)
+        gainReqGroup = nbRequests * (latToDatacenter - minLatTotal)
+        sumGains += gainReqGroup
+        sumNbReqs += nbRequests
+    ratio = sumGains/sumNbReqs
+    return int(ratio*1000)
 
-
-
-for line in sys.stdin:
-    print(line)
-
-
-
+if __name__ == '__main__':
+    inputs = parseInputs()
+    videosByCache = parseSolution()
+    score = scoreSolution(inputs,videosByCache)
+    print("score = ",score)
