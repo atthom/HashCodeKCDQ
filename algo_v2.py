2e70fffa52a227c114fee4f14db683556c749bb3
Günther Jungbluth <gunther@gameslabs.net>
2017-02-23 19:59:29 +0100

    random stuff

--- algo_v2.py
+++ algo_v2.py
@@ -1,13 +1,10 @@
 
-def algo(cache_size, nb_cache, endpoints, video_sizes):
+def algo(cache_size, nb_cache, endpoints):
     # endpoint is : (latency, [(idCache, latencyCache)], [(idVideo, requests])
     # for each enpoints, sort videos by requests
     for endpoint in endpoints:
         endpoint[2] = sorted(endpoint[2], key=video_sorter, reverse=True)
 
-    caches = []
-    for i in range(nb_cache):
-        caches[i] = []
 
     return []
 
